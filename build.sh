19e4256e1b273d5cbae3051089c5aff3158c01af
Jasper Aorangi <jasper.mackenzie@gmail.com>
2019-05-26 19:11:03 +1200

    * Update icons and background colour * Minor changes to build.sh

--- build.sh
+++ build.sh
@@ -9,11 +9,11 @@ ANTENNAPOD_LOG_FILE="${ANTENNAPOD_LOG_FILE:-/tmp/build_antennapod.log}"
 
 if [[ $? -eq 0 ]]
 then
-        echo "BUILD SUCCESSFUL!"
         ./gradlew --stop
+        echo "BUILD SUCCESSFUL!"
         exit 0
 else
-        echo "FAIL"
         ./gradlew --stop
+        echo "FAIL"
         exit 1
 fi
